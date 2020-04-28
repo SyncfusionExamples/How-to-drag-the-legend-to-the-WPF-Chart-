e18853b7fd16080277e6fcbbfbde7bd5e2fbffdc
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-04-28 19:44:35 +0530

    Sample included

---
+++ Sample-WPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isDragable = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void SfChart_MouseMove(object sender, MouseEventArgs e)
+        {
+            var chart = sender as SfChart;
+            if (isDragable)
+            {
+                var position = e.GetPosition(chart);
+                ChartLegend legend = chart.Legend as ChartLegend;
+                legend.OffsetX = position.X - (chart.SeriesClipRect.Left + legend.DesiredSize.Width / 2);
+                legend.OffsetY = position.Y - (chart.SeriesClipRect.Top + legend.DesiredSize.Height / 2);
+
+                if ((chart.Legend as ChartLegend).IsMouseOver)
+                {
+                    if (Mouse.OverrideCursor == null)
+                        Mouse.OverrideCursor = Cursors.Hand;
+                }
+            }
+
+        }
+
+        private void SfChart_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragable = false;
+        }
+
+        private void Legend_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragable = true;
+        }
+    }
+}
---
+++ Sample-WPF/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_WPF
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<Model>();
+            Data.Add(new Model() { XValue = 1, YValue1 = 8 });
+            Data.Add(new Model() { XValue = 2, YValue1 = 5 });
+            Data.Add(new Model() { XValue = 3, YValue1 = 3 });
+            Data.Add(new Model() { XValue = 4, YValue1 = 9 });
+            Data.Add(new Model() { XValue = 5, YValue1 = 6 });
+            Data.Add(new Model() { XValue = 6, YValue1 = 7 });
+        }
+    }
+
+    public class Model
+    {
+        public double XValue { get; set; }
+
+        public double YValue1 { get; set; }
+
+        public double YValue2 { get; set; }
+    }
+}
